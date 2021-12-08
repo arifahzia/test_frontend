afdcc6338fcb5e22e0034dcc6ccee42c8af93a2b
Arifahzia <64831005+arifahzia@users.noreply.github.com>
2021-12-08 15:41:24 +0700

    Add files via upload

---
+++ js/script-web.js
@@ -0,0 +1,44 @@
+DATA_BUKU = [
+	{
+		image: "assets/img/kategori/cantikitu.png",
+		judul: "Cantik Itu Luka",
+		ket: "Novel berjudul Cantik Itu Luka karya Eka Kurniawan mengangkat tema seorang perempuan di masa kolonial. Perempuan itu bernama Dewi Ayu. Ia cantik dan dipaksa menjadi seorang pelacur. Pekerjaannya itu membuat dirinya tumbuh menjadi wanita pelacur yang tersohor di Halimunda.",
+		harga: 18000
+	},
+	{
+		image: "assets/img/kategori/senjahujan.png",
+		judul: "Senja dan Hujan",
+		ket: "Senja, Hujan dan Cerita yang Telah Usai",
+		harga: 23000
+	},
+	{
+		image: "assets/img/kategori/kisahtanahjawa.png",
+		judul: "Kisah Tanah Jawa",
+		ket: "Tanah jawa menyimpan banyak kisah misteri yang takkan habis di ceritakan dalam semalam. Sosok misterius, ritual mistis, dan tempat angker, selalu membuat penasaran. Buku kisah tanah jawa ini mengajak pambaca membuka selubung mitos dan mistis yang selama ini hanya menjadi kasak- kusuk di masyarakat.",
+		harga: 24000
+	},
+	{
+		image: "assets/img/kategori/antares.png",
+		judul: "Antares",
+		ket: "Antares bercerita tentang dua remaja SMA yang saling jatuh cinta. Di sini ada tokoh bernama Antares Sebastian Aldevaro alias Antares itu sendiri. Kemudian ada juga tokoh Zeanne Queensha Bratadikara yang kerap dipanggil Zea. Antares dan Zea dipertemukan ketika Zea pindah ke sekolah tempat Antares berada.",
+		harga: 17000
+	},
+	{
+		image: "assets/img/kategori/senjadanpagi.png",
+		judul: "Senja Dan Pagi",
+		ket: "Buku ini merupakan kisah perjalanan Alffy Rev dan Linka Angelia.Novel ini menceritakan tentang perjalanan cinta mereka yang di awali dengan pertemuan dua pribadi yang berbeda namun memiliki mimpi yang sama.",
+		harga: 23000
+	},
+	{
+		image: "assets/img/kategori/layanganputus.png",
+		judul: "layangan Putus",
+		ket: "Seorang gadis remaja polos yang berasal dari daerah, tumbuh, berkembang, dan menemukan cinta di kota besar yang sangat berbeda",
+		harga: 24000
+	},
+]
+var app = new Vue({
+	el: "#app",
+	data: {
+		dataBuku: DATA_BUKU
+	}
+})
